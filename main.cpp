8b3bc280436a2fb20b1d3c8cef924089ab3bf94f
raito159 <berraki_5@hotmail.com>
2020-06-25 18:06:11 +0200

    Little commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int i = 0;
+
+    for(i; i < 10; i++)
+    {
+        cout << "Hello world number " << i << " !" << endl;
+    }
+    
+    return 0;
+}
