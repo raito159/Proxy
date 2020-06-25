5b1b263362ee4775147181249c9050dd5ce4ffc3
raito159 <berraki_5@hotmail.com>
2020-06-25 21:25:32 +0200

    5th

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     for(i; i < 10; i++)
     {
         cout << "Hello world number " << i << " !" << endl;
-        cout << "FOURTH" << end;
+        cout << "" << endl;
     }
     
     return 0;
